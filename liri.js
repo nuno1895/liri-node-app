5abe8bfd33e48c27b0d7431b467e2bb424254c4a
Nuno Tavares <Nuno@Nunos-MacBook-Pro.local>
2017-02-01 13:49:08 -0500

    added colors

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 var twitter = require("twitter");
 var spotify = require("spotify");
 var request = require("request");
+var colors = require('colors')
 
 // var to hold location of required file
 var keyFile = require("./keys.js");
@@ -51,7 +52,7 @@ if (command == "my-tweets") {
     client.get("statuses/user_timeline", function(err, tweets) {
         if (!err) {
             for (var tweet = 0; tweet < tweets.length; tweet++) {
-                console.log(tweets[tweet].text);
+                console.log(tweets[tweet].text.blue);
             };
         } else {
             throw err;
@@ -107,9 +108,9 @@ function log(file, command) {
 
 // function to retrieve song info
 function songInfo(songName) {
-    console.log("\n");
-    console.log("The Song Name: " + songName.toUpperCase());
-    console.log("\n");
+    console.log("\n".green);
+    console.log("The Song Name: ".green + songName.toUpperCase().green);
+    console.log("\n".green);
     spotify.search({type: "track", query: songName}, function(err, data) {
         if (err) {
             console.log("Error: " + err);
@@ -117,12 +118,12 @@ function songInfo(songName) {
             return;
         } else {
             for (var i = 0; i < data.tracks.items.length; i++) {
-                console.log("--------------------------------------------------------------------");
-                console.log("Search Results: #" + (i))
-                console.log("Artist: " + data.tracks.items[i].artists[0].name);
-                console.log("Song Name: " + data.tracks.items[i].name);
-                console.log("Album: " + data.tracks.items[i].album.name);
-                console.log("Preview Link: " + data.tracks.items[i].external_urls.spotify + "\n");
+                console.log("--------------------------------------------------------------------".green);
+                console.log("Search Results: #".magenta + i)
+                console.log("Artist: ".magenta + data.tracks.items[i].artists[0].name.green);
+                console.log("Song Name: ".magenta + data.tracks.items[i].name.green);
+                console.log("Album: ".magenta + data.tracks.items[i].album.name.green);
+                console.log("Preview Link: ".magenta + data.tracks.items[i].external_urls.spotify.green + "\n".green);
             };
         };
     });
@@ -134,16 +135,16 @@ function movieInfo(movieURL) {
         if (!err && response.statusCode == 200) {
             //convert body to string
             body = JSON.parse(body);
-            console.log("\n");
-            console.log("Title: " + body.Title);
-            console.log("Year: " + body.Year);
-            console.log("Rating: " + body.Rated);
-            console.log("Country: " + body.Country);
-            console.log("Language: " + body.Language);
-            console.log("Plot: " + body.Plot);
-            console.log("Cast: " + body.Actors);
-            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-            console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+            console.log("\n".red);
+            console.log("Title: ".blue + body.Title.red);
+            console.log("Year: ".blue + body.Year.red);
+            console.log("Rating: ".blue + body.Rated.red);
+            console.log("Country: ".blue + body.Country.red);
+            console.log("Language: ".blue + body.Language.red);
+            console.log("Plot: ".blue + body.Plot.red);
+            console.log("Cast: ".blue + body.Actors.red);
+            console.log("Rotten Tomatoes Rating: ".blue + body.tomatoRating.red);
+            console.log("Rotten Tomatoes URL: ".blue + body.tomatoURL.red);
             console.log("\n");
         } else {
             log("error.txt", command + " Error: " + err);
